c71b69161e8d104d14ecb3142137513f38377c7d
Nick Buikema <nick.buikema@gmail.com>
2019-10-22 12:32:11 -0500

    connected database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('database connected');
+});
 
 app.get('/', (req, res) => {
     res.send('init');
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('server started on port 3000');
 });
