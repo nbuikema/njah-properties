b5f9c37906108e3d37bd8c57ffd1965d8aa4f83d
Nick Buikema <nick.buikema@gmail.com>
2019-10-26 13:51:40 -0500

    added read all users logic

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,3 +3,12 @@ const User = require('../models/user');
 exports.readCurrentUser = (req, res) => {
     return res.json({user: req.user});
 };
+
+exports.readAllUsers = (req, res) => {
+    User.find().exec((err, users) => {
+        if(err) {
+            return res.status(400).json({error: 'Could not find users.'});
+        }
+        return res.json(users);
+    });
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const {isAuth} = require('../controllers/auth');
-const {readCurrentUser} = require('../controllers/user');
+const {isAuth, isAdmin} = require('../controllers/auth');
+const {readCurrentUser, readAllUsers} = require('../controllers/user');
 
 router.get('/read/current', isAuth, readCurrentUser);
+router.get('/read/all', isAuth, isAdmin, readAllUsers);
 
 module.exports = router;
