f204aec35d0d1cb71cab92677080f1d1f5dee8d7
Nick Buikema <nick.buikema@gmail.com>
2019-10-27 15:36:58 -0500

    initial properties model

---
+++ api/models/property.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const propertySchema = new mongoose.Schema(
+    {
+        address: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        city: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        state: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        zip: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        lat: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        long: {
+            type: String,
+            trim: true,
+            required: true
+        }
+    }, {timestamps: true}
+);
+
+module.exports = mongoose.model('Property', propertySchema);
