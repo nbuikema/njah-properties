5b7febb73b5557e31a1ab964ecb1d6c9f2748f96
Nick Buikema <nick.buikema@gmail.com>
2019-10-27 18:30:39 -0500

    conditionally render markers from properties in db

---
+++ client/src/components/properties/Mapbox.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
+
+import markerImg from './marker.svg';
+
+const Mapbox = ({properties}) => {
+    const Map = ReactMapboxGl({
+        minZoom: 9,
+        maxZoom: 16,
+        accessToken: process.env.REACT_APP_MAPBOX_KEY
+    });
+    const zoom = [11.5];
+    const center = [-97.1031, 33.2148];
+    const layoutLayer = {'icon-image': 'marker'};
+    const image = new Image();
+    image.src = markerImg;
+    const images = ['marker', image];
+
+    return (
+        <Map style="mapbox://styles/mapbox/streets-v8" zoom={zoom} center={center} containerStyle={{position: "fixed", height: "100vh", width: "100%"}}>
+            <Layer type="symbol" id="marker" layout={layoutLayer} images={images}>
+                {properties.map((property, i) => {
+                    let coordinates = [Number(property.long), Number(property.lat)];
+                    return (
+                        <Feature key={i} coordinates={coordinates} />
+                    );
+                })}
+            </Layer>
+        </Map>
+    );
+};
+
+export default Mapbox;
--- client/src/components/properties/Properties.js
+++ client/src/components/properties/Properties.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
+import React, {useState, useEffect} from 'react';
+import {readAllProperties} from './apiProperties';
 
-import markerImg from './marker.svg';
+import Mapbox from './Mapbox';
 
 const Properties = () => {
-    const Map = ReactMapboxGl({
-        minZoom: 9,
-        maxZoom: 16,
-        accessToken: process.env.REACT_APP_MAPBOX_KEY
-    });
-    const zoom = [11.5];
-    const center = [-97.1031, 33.2148];
-    const layoutLayer = {'icon-image': 'marker'};
-    const image = new Image();
-    image.src = markerImg;
-    const images = ['marker', image];
+    const [properties, setProperties] = useState([]);
+
+    const getAllProperties = () => {
+        readAllProperties().then(data => {
+            setProperties(data);
+        });
+    };
+
+    useEffect(() => {
+        getAllProperties();
+    }, []);
 
     return (
         <div>
             <div className='row'>
                 <div className='col-8 p-0 fixed'>
-                    <Map style="mapbox://styles/mapbox/streets-v8" zoom={zoom} center={center} containerStyle={{position: "fixed", height: "100vh", width: "100%"}}>
-                        <Layer type="symbol" id="marker" layout={layoutLayer} images={images}>
-                            <Feature coordinates={[-97.110277, 33.219144]}/>
-                        </Layer>
-                    </Map>
+                    {properties.length > 0 && <Mapbox properties={properties} />}
                 </div>
                 <div className='col-4 p-0'>
                     <div className="card">
@@ -32,7 +28,6 @@ const Properties = () => {
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                     <div className="card">
@@ -40,7 +35,6 @@ const Properties = () => {
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                     <div className="card">
@@ -48,7 +42,6 @@ const Properties = () => {
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                     <div className="card">
@@ -56,7 +49,6 @@ const Properties = () => {
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                     <div className="card">
@@ -64,7 +56,6 @@ const Properties = () => {
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
