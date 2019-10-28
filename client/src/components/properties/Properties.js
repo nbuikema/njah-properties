56128d36514f0c6e14ffa38df6b180cef088ba92
Nick Buikema <nick.buikema@gmail.com>
2019-10-27 23:13:25 -0500

    fixed many issues with map layers

--- client/src/components/properties/Properties.js
+++ client/src/components/properties/Properties.js
@@ -1,10 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import {readAllProperties} from './apiProperties';
-
-import Mapbox from './Mapbox';
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 const Properties = () => {
     const [properties, setProperties] = useState([]);
+    const [selected, setSelected] = useState(null);
+    const [viewport, setViewport] = useState({
+        longitude: -97.1331,
+        latitude: 33.2148,
+        width: '100%',
+        height: 'calc(100vh - 60px)',
+        zoom: 11
+    });
 
     const getAllProperties = () => {
         readAllProperties().then(data => {
@@ -16,11 +23,24 @@ const Properties = () => {
         getAllProperties();
     }, []);
 
+    const changeSelected = id => event => {
+        event.preventDefault();
+        setSelected(id);
+    }
+
     return (
         <div>
             <div className='row'>
                 <div className='col-8 p-0 fixed'>
-                    {properties.length > 0 && <Mapbox properties={properties} />}
+                    <ReactMapGL {...viewport} mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY} onViewportChange={viewport => {setViewport(viewport)}}>
+                        {properties.map(property => (
+                            <Marker key={property._id} latitude={Number(property.lat)} longitude={Number(property.long)}>
+                                <button class='markerbtn' onClick={changeSelected(`${property._id}`)}>
+                                    <div class='marker'></div>
+                                </button>
+                            </Marker>
+                        ))}
+                    </ReactMapGL>
                 </div>
                 <div className='col-4 p-0'>
                     <div className="card">
