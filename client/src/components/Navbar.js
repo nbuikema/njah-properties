a8be980a5dff31249caf80aa42ca3886fcfc4ca5
Nick Buikema <nick.buikema@gmail.com>
2019-10-31 15:03:45 -0500

    centered navbar logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = ({history}) => (
     <div className='sticky-top'>
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
             <div className='container'>
-                <ul className='navbar-nav mr-auto mt-lg-0'>
+                <ul className='navbar-nav mr-auto mt-lg-0 d-block d-lg-none'>
                     <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                         <Link className='navbar-brand' to='/'>
                             NJAH Properties
@@ -25,11 +25,25 @@ const Navbar = ({history}) => (
                 <div className='collapse navbar-collapse' id='navbarToggler'>
                     <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
                         <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                            <Link className='nav-link' to='/' style={isActive(history, '/')}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                             <Link className='nav-link' to='/properties' style={isActive(history, '/properties')}>
                                 Properties
                             </Link>
                         </li>
                     </ul>
+                </div>
+                <ul className='navbar-nav mx-auto mt-lg-0 d-none d-lg-block'>
+                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                        <Link className='navbar-brand' to='/'>
+                            NJAH Properties
+                        </Link>
+                    </li>
+                </ul>
+                <div className='collapse navbar-collapse' id='navbarToggler'>
                     <div className='dropdown-divider'></div>
                     {!isAuth() ? (
                         <ul className='navbar-nav ml-auto mt-2 mt-lg-0'>
