155693248ea11e203f6efa112e42bfe58307a3c3
Nick Buikema <nick.buikema@gmail.com>
2019-11-03 11:57:28 -0600

    updated contact side info

--- client/src/components/core/Contact.js
+++ client/src/components/core/Contact.js
@@ -18,7 +18,7 @@ const Contact = () => {
     };
 
     const contactForm = () => (
-        <form className='mt-3'>
+        <form>
             <div className='form-group'>
                 <label htmlFor='name'>Name</label>
                 <input onChange={onChange('name')} value={name} type='text' className='form-control' id='name' />
@@ -32,16 +32,42 @@ const Contact = () => {
                 <textarea onChange={onChange('message')} value={message} rows='4' className='form-control' id='message' aria-describedby='message'></textarea>
             </div>
             <div className='text-center'>
-                <button onClick={onSubmit} type='submit' className='btn btn-primary-inverse mb-3'>Get In Touch</button>
+                <button onClick={onSubmit} type='submit' className='btn btn-primary'>Get In Touch</button>
             </div>
         </form>
     );
 
     return (
         <div className='container'>
-            <h1 className='text-center mt-3'>Contact Me</h1>
-            <h4 className='text-center'>creationsfromcindy@gmail.com</h4>
-            {contactForm()}
+            <div className='row'>
+                <div className='col-8'>
+                    <h2>Send Us A Message</h2>
+                    <br />
+                    {contactForm()}
+                </div>
+                <div className='col-4'>
+                    <div>
+                        <h2>Forms</h2>
+                        <br />
+                        <a href=''>Application</a>
+                    </div>
+                    <br />
+                    <hr />
+                    <br />
+                    <div>
+                        <h2>Contact Info</h2>
+                        <br />
+                        <h6>Telephone</h6>
+                        <a href="tel:1234567890">123-456-7890</a>
+                        <hr />
+                        <h6>Fax</h6>
+                        <a href="tel:1234567890">123-456-7890</a>
+                        <hr />
+                        <h6>Email</h6>
+                        <a href="mailto:sample@email.com">sample@email.com</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
