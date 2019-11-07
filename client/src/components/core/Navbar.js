e647d942f0f602fab72ab6077d5a1174772a3dc5
Nick Buikema <nick.buikema@gmail.com>
2019-11-07 14:38:03 -0600

    updated navbar

--- client/src/components/core/Navbar.js
+++ client/src/components/core/Navbar.js
@@ -10,86 +10,18 @@ const isActive = (history, path) => {
 
 const Navbar = ({history}) => (
     <div className='sticky-top'>
-        <nav class="navbar navbar-expand-md navbar-light bg-light">
-            <div className='navbar-brand abs'>
-                <Link className='brand-fix' to='/'>
-                    NJAH Properties
-                </Link>
-            </div>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="navbar-collapse collapse" id="collapsingNavbar">
-                <ul class="navbar-nav">
-                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                        <Link className={`nav-link ${isActive(history, '/') && 'active'}`} to='/'>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                        <Link className={`nav-link ${isActive(history, '/properties') && 'active'}`} to='/properties'>
-                            Properties
-                        </Link>
-                    </li>
-                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                        <Link className={`nav-link ${isActive(history, '/contact') && 'active'}`} to='/contact'>
-                            Contact & Forms
-                        </Link>
-                    </li>
-                </ul>
-                <ul class="navbar-nav ml-auto">
-                    {!isAuth() ? (
-                        <>
-                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                                <Link className={`nav-link ${isActive(history, '/signup') && 'active'}`} to='/signup'>
-                                    Sign Up
-                                </Link>
-                            </li>
-                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                                <Link className={`nav-link ${isActive(history, '/signin') && 'active'}`} to='/signin'>
-                                    Sign In
-                                </Link>
-                            </li>
-                        </>
-                    ) : (
-                        <>
-                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                                <Link className={`nav-link ${isActive(history, '/dashboard') && 'active'}`} to='/dashboard'>
-                                    Dashboard
-                                </Link>
-                            </li>
-                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                                <span className='nav-link' style={{cursor: 'pointer'}} onClick={() => signout(() => {history.push('/');})}>
-                                    Sign Out
-                                </span>
-                            </li>
-                        </>
-                    )}
-                </ul>
-            </div>
-        </nav>
-    </div>
-);
-
-export default withRouter(Navbar);
-
-
-
-/*
-<nav className='navbar navbar-expand-lg navbar-light bg-light'>
-            <div className='container'>
-                <ul className='navbar-nav mr-auto mt-lg-0 d-block d-lg-none'>
-                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                        <Link className='navbar-brand' to='/'>
-                            NJAH Properties
-                        </Link>
-                    </li>
-                </ul>
-                <button className='navbar-toggler custom-toggler' type='button' data-toggle='collapse' data-target='#navbarToggler' aria-controls='navbarToggler' aria-expanded='false' aria-label='Toggle Navigation'>
-                    <span className='navbar-toggler-icon'></span>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div className='container-fluid'>
+                <div className='navbar-brand abs'>
+                    <Link className='brand-fix' to='/'>
+                        NJAH Properties
+                    </Link>
+                </div>
+                <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <div className='collapse navbar-collapse' id='navbarToggler'>
-                    <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+                <div class="navbar-collapse collapse" id="collapsingNavbar">
+                    <ul class="navbar-nav">
                         <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                             <Link className={`nav-link ${isActive(history, '/') && 'active'}`} to='/'>
                                 Home
@@ -106,46 +38,39 @@ export default withRouter(Navbar);
                             </Link>
                         </li>
                     </ul>
-                </div>
-                <ul className='navbar-nav mx-auto mt-lg-0 d-none d-lg-block'>
-                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                        <Link className='navbar-brand' to='/'>
-                            NJAH Properties
-                        </Link>
-                    </li>
-                </ul>
-                <div className='collapse navbar-collapse' id='navbarToggler'>
-                    <div className='dropdown-divider'></div>
-                    {!isAuth() ? (
-                        <ul className='navbar-nav ml-auto mt-2 mt-lg-0'>
-                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                                <Link className={`nav-link ${isActive(history, '/signup') && 'active'}`} to='/signup'>
-                                    Sign Up
-                                </Link>
-                            </li>
-                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                                <Link className={`nav-link ${isActive(history, '/signin') && 'active'}`} to='/signin'>
-                                    Sign In
-                                </Link>
-                            </li>
-                        </ul>
-                    ) : (
-                        <ul className='navbar-nav ml-auto mt-2 mt-lg-0'>
-                            {isAuth() ? (
+                    <ul class="navbar-nav ml-auto">
+                        {!isAuth() ? (
+                            <>
+                                <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                                    <Link className={`nav-link ${isActive(history, '/signup') && 'active'}`} to='/signup'>
+                                        Sign Up
+                                    </Link>
+                                </li>
+                                <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                                    <Link className={`nav-link ${isActive(history, '/signin') && 'active'}`} to='/signin'>
+                                        Sign In
+                                    </Link>
+                                </li>
+                            </>
+                        ) : (
+                            <>
                                 <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
                                     <Link className={`nav-link ${isActive(history, '/dashboard') && 'active'}`} to='/dashboard'>
                                         Dashboard
                                     </Link>
                                 </li>
-                            ) : null}
-                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
-                                <span className='nav-link' style={{cursor: 'pointer'}} onClick={() => signout(() => {history.push('/');})}>
-                                    Sign Out
-                                </span>
-                            </li>
-                        </ul>
-                    )}
+                                <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                                    <span className='nav-link' style={{cursor: 'pointer'}} onClick={() => signout(() => {history.push('/');})}>
+                                        Sign Out
+                                    </span>
+                                </li>
+                            </>
+                        )}
+                    </ul>
                 </div>
             </div>
         </nav>
-        */
+    </div>
+);
+
+export default withRouter(Navbar);
