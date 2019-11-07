0d3bb445f9368bf22f3691cd26922edb77e6a506
Nick Buikema <nick.buikema@gmail.com>
2019-11-07 14:31:20 -0600

    centered navbar logo on large screens

--- client/src/components/core/Navbar.js
+++ client/src/components/core/Navbar.js
@@ -10,7 +10,73 @@ const isActive = (history, path) => {
 
 const Navbar = ({history}) => (
     <div className='sticky-top'>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <nav class="navbar navbar-expand-md navbar-light bg-light">
+            <div className='navbar-brand abs'>
+                <Link className='brand-fix' to='/'>
+                    NJAH Properties
+                </Link>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse" id="collapsingNavbar">
+                <ul class="navbar-nav">
+                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                        <Link className={`nav-link ${isActive(history, '/') && 'active'}`} to='/'>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                        <Link className={`nav-link ${isActive(history, '/properties') && 'active'}`} to='/properties'>
+                            Properties
+                        </Link>
+                    </li>
+                    <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                        <Link className={`nav-link ${isActive(history, '/contact') && 'active'}`} to='/contact'>
+                            Contact & Forms
+                        </Link>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    {!isAuth() ? (
+                        <>
+                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                                <Link className={`nav-link ${isActive(history, '/signup') && 'active'}`} to='/signup'>
+                                    Sign Up
+                                </Link>
+                            </li>
+                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                                <Link className={`nav-link ${isActive(history, '/signin') && 'active'}`} to='/signin'>
+                                    Sign In
+                                </Link>
+                            </li>
+                        </>
+                    ) : (
+                        <>
+                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                                <Link className={`nav-link ${isActive(history, '/dashboard') && 'active'}`} to='/dashboard'>
+                                    Dashboard
+                                </Link>
+                            </li>
+                            <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
+                                <span className='nav-link' style={{cursor: 'pointer'}} onClick={() => signout(() => {history.push('/');})}>
+                                    Sign Out
+                                </span>
+                            </li>
+                        </>
+                    )}
+                </ul>
+            </div>
+        </nav>
+    </div>
+);
+
+export default withRouter(Navbar);
+
+
+
+/*
+<nav className='navbar navbar-expand-lg navbar-light bg-light'>
             <div className='container'>
                 <ul className='navbar-nav mr-auto mt-lg-0 d-block d-lg-none'>
                     <li className='nav-item' data-toggle='collapse' data-target='.navbar-collapse.show'>
@@ -82,7 +148,4 @@ const Navbar = ({history}) => (
                 </div>
             </div>
         </nav>
-    </div>
-);
-
-export default withRouter(Navbar);
+        */
