48f6ded989d9dd4b95a98f5191b7fb983321cee2
Nick Buikema <nick.buikema@gmail.com>
2019-12-08 19:13:11 -0600

    remove italicized text

--- client/src/components/users/Messages.js
+++ client/src/components/users/Messages.js
@@ -171,21 +171,21 @@ const Messages = ({role}) => {
                     {i !== 0 && <hr />}
                     <div className='row'>
                         <div className='col-6'>
-                            <h6><strong>Name:</strong> <em>{message.last_name}, {message.first_name}</em></h6>
-                            <h6><strong>Email:</strong> <em>{message.email}</em></h6>
-                            <h6><strong>Phone:</strong> <em>{message.phone}</em></h6>
-                            <h6><strong>Sent:</strong> <em>{moment(message.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</em></h6>
+                            <h6><strong>Name:</strong> {message.last_name}, {message.first_name}</h6>
+                            <h6><strong>Email:</strong> {message.email}</h6>
+                            <h6><strong>Phone:</strong> {message.phone}</h6>
+                            <h6><strong>Sent:</strong> {moment(message.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</h6>
                         </div>
                         <div className='col-6'>
-                            <h6><strong>Type:</strong> <em>{message.type}</em></h6>
-                            <h6><strong>Reason:</strong> <em>{message.reason}</em></h6>
+                            <h6><strong>Type:</strong> {message.type}</h6>
+                            <h6><strong>Reason:</strong> {message.reason}</h6>
                             {message.property && (
-                                <h6><strong>Property:</strong> <em>{message.property.address}, {message.property.city}, {message.property.state}, {message.property.zip}</em></h6>
+                                <h6><strong>Property:</strong> {message.property.address}, {message.property.city}, {message.property.state}, {message.property.zip}</h6>
                             )}
                             {message.application && (
-                                <h6><strong>Application:</strong> <em><a href={`${message.application.url}`} target='_blank' rel="noopener noreferrer">View Application</a></em></h6>
+                                <h6><strong>Application:</strong> <a href={`${message.application.url}`} target='_blank' rel="noopener noreferrer">View Application</a></h6>
                             )}
-                            <h6><strong>Message:</strong> <em>{message.message}</em></h6>
+                            <h6><strong>Message:</strong> {message.message}</h6>
                         </div>
                     </div>
                 </div>
@@ -198,7 +198,7 @@ const Messages = ({role}) => {
                             <h6><strong>Name:</strong> {message.last_name}, {message.first_name}</h6>
                             <h6><strong>Email:</strong> {message.email}</h6>
                             <h6><strong>Phone:</strong> {message.phone}</h6>
-                            <h6><strong>Sent:</strong> <em>{moment(message.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</em></h6>
+                            <h6><strong>Sent:</strong> {moment(message.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</h6>
                         </div>
                         <div className='col-6'>
                             <h6><strong>Type:</strong> {message.type}</h6>
--- client/src/components/users/UserInfo.js
+++ client/src/components/users/UserInfo.js
@@ -23,37 +23,37 @@ const UserInfo = ({user}) => {
                     <h6 className='form-group row'>
                         <label className='col-sm-4 col-form-label'><strong>First Name</strong></label>
                         <div className='col-sm-8'>
-                            <span className='form-control-plaintext text-primary'><em>{first_name}</em></span>
+                            <span className='form-control-plaintext text-primary'>{first_name}</span>
                         </div>
                     </h6>
                     <h6 className='form-group row'>
                         <label className='col-sm-4 col-form-label'><strong>Last Name</strong></label>
                         <div className='col-sm-8'>
-                            <span className='form-control-plaintext text-primary'><em>{last_name}</em></span>
+                            <span className='form-control-plaintext text-primary'>{last_name}</span>
                         </div>
                     </h6>
                     <h6 className='form-group row'>
                         <label className='col-sm-4 col-form-label'><strong>Email</strong></label>
                         <div className='col-sm-8'>
-                            <span className='form-control-plaintext text-primary'><em>{email}</em></span>
+                            <span className='form-control-plaintext text-primary'>{email}</span>
                         </div>
                     </h6>
                     <h6 className='form-group row'>
                         <label className='col-sm-4 col-form-label'><strong>Phone</strong></label>
                         <div className='col-sm-8'>
-                            <span className='form-control-plaintext text-primary'><em>{phone}</em></span>
+                            <span className='form-control-plaintext text-primary'>{phone}</span>
                         </div>
                     </h6>
                     <h6 className='form-group row'>
                         <label className='col-sm-4 col-form-label'><strong>Joined</strong></label>
                         <div className='col-sm-8'>
-                            <span className='form-control-plaintext text-primary'><em>{moment(createdAt).format('MMMM Do YYYY, h:mm:ss a')}</em></span>
+                            <span className='form-control-plaintext text-primary'>{moment(createdAt).format('MMMM Do YYYY, h:mm:ss a')}</span>
                         </div>
                     </h6>
                     <h6 className='form-group row'>
                         <label className='col-sm-4 col-form-label'><strong>Last Updated</strong></label>
                         <div className='col-sm-8'>
-                            <span className='form-control-plaintext text-primary'><em>{moment(updatedAt).format('MMMM Do YYYY, h:mm:ss a')}</em></span>
+                            <span className='form-control-plaintext text-primary'>{moment(updatedAt).format('MMMM Do YYYY, h:mm:ss a')}</span>
                         </div>
                     </h6>
                 </div>
@@ -62,31 +62,31 @@ const UserInfo = ({user}) => {
                         <h6 className='form-group row'>
                             <label className='col-sm-4 col-form-label'><strong>Address</strong></label>
                             <div className='col-sm-8'>
-                                <span className='form-control-plaintext text-primary'><em>{property.address}, {property.city}, {property.state}, {property.zip}</em></span>
+                                <span className='form-control-plaintext text-primary'>{property.address}, {property.city}, {property.state}, {property.zip}</span>
                             </div>
                         </h6>
                         <h6 className='form-group row'>
                             <label className='col-sm-4 col-form-label'><strong>Rent</strong></label>
                             <div className='col-sm-8'>
-                                <span className='form-control-plaintext text-primary'><em>${property.rent}</em></span>
+                                <span className='form-control-plaintext text-primary'>${property.rent}</span>
                             </div>
                         </h6>
                         <h6 className='form-group row'>
                             <label className='col-sm-4 col-form-label'><strong>Size</strong></label>
                             <div className='col-sm-8'>
-                                <span className='form-control-plaintext text-primary'><em>{property.size} Sq Ft</em></span>
+                                <span className='form-control-plaintext text-primary'>{property.size} Sq Ft</span>
                             </div>
                         </h6>
                         <h6 className='form-group row'>
                             <label className='col-sm-4 col-form-label'><strong>Beds</strong></label>
                             <div className='col-sm-8'>
-                                <span className='form-control-plaintext text-primary'><em>{property.beds}</em></span>
+                                <span className='form-control-plaintext text-primary'>{property.beds}</span>
                             </div>
                         </h6>
                         <h6 className='form-group row'>
                             <label className='col-sm-4 col-form-label'><strong>Baths</strong></label>
                             <div className='col-sm-8'>
-                                <span className='form-control-plaintext text-primary'><em>{property.baths}</em></span>
+                                <span className='form-control-plaintext text-primary'>{property.baths}</span>
                             </div>
                         </h6>
                         <h6 className='form-group row'>
