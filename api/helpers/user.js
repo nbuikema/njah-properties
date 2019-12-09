fc06a854fc18dee6b687f94fe3c87067ae225b82
Nick Buikema <nick.buikema@gmail.com>
2019-12-09 15:41:28 -0600

    added update self validation

--- api/helpers/user.js
+++ api/helpers/user.js
@@ -11,3 +11,23 @@ exports.updateUserValidator = (req, res, next) => {
     }
     next();
 };
+
+exports.updateSelfValidator = (req, res, next) => {
+    req.check('first_name', 'First name is required.').notEmpty();
+    req.check('last_name', 'Last name is required.').notEmpty();
+    req.check('email', 'Email is required.').notEmpty();
+    req.check('email', 'Invalid email format.').matches(/.+\@.+\..+/);
+    req.check('phone', 'Phone number is required.').notEmpty();
+    if(req.body.password.length > 0) {
+        req.check('password', 'Password is required.').notEmpty();
+        req.check('password', 'Password must contain at least 6 characters.').isLength({min: 6});
+        req.check('password', 'Password must contain a number.').matches(/\d/);
+        req.check('password', 'Password must contain a special character.').matches(/[-!$%^&*()_+|~=`{}[:;<>?,.@#\]]/);
+    }
+    const errors = req.validationErrors();
+    if(errors) {
+        const errorList = errors.map(error => error.msg)[0];
+        return res.status(400).json({err: errorList});
+    }
+    next();
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {isAuth, isAdmin, isSelf} = require('../controllers/auth');
+const {isAuth, isAdmin} = require('../controllers/auth');
 const {userById, readCurrentUser, readAllUsers, updateUser, deleteUser, updateSelf} = require('../controllers/user');
-const {updateUserValidator} = require('../helpers/user');
+const {updateUserValidator, updateSelfValidator} = require('../helpers/user');
 
 router.get('/read/current', isAuth, readCurrentUser);
 router.get('/read/all', isAuth, isAdmin, readAllUsers);
 router.put('/update/:userId', isAuth, isAdmin, updateUserValidator, updateUser);
 router.delete('/delete/:userId', isAuth, isAdmin, deleteUser);
-router.put('/update/self/:userId', isAuth, updateSelf);
+router.put('/update/self/:userId', isAuth, updateSelfValidator, updateSelf);
 
 router.param('userId', userById);
 
