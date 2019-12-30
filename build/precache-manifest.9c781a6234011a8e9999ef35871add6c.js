5395e2f9f64b699683fbc8af1b87675cf2ad0d56
Nick Buikema <nick.buikema@gmail.com>
2019-12-30 15:14:03 -0600

    deploy v1

--- build/precache-manifest.9c781a6234011a8e9999ef35871add6c.js
+++ build/precache-manifest.9c781a6234011a8e9999ef35871add6c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4153c8855d9359cd3b90063b35ea4b01",
+    "revision": "3671a15ff07ca8e397a4b30d2d2584f7",
     "url": "/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5705454a.chunk.js.LICENSE"
   },
   {
-    "revision": "9900ea9c1bb56dda418a",
-    "url": "/static/js/main.a5f6a76a.chunk.js"
+    "revision": "09eece1d85436ce66a38",
+    "url": "/static/js/main.2196e691.chunk.js"
   },
   {
     "revision": "2bcb8084869f23f72706",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bfcca8a9e3b9c623701c009b064480b.js"
+  "/precache-manifest.9c781a6234011a8e9999ef35871add6c.js"
 );
 
 self.addEventListener('message', (event) => {
