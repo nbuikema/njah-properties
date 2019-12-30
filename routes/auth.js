30dcec5af15c92d6e76ea805bed1af252161a19b
Nick Buikema <nick.buikema@gmail.com>
2019-12-30 15:02:27 -0600

    remove admin requirement to add user

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {signup, signin, isAuth, isAdmin, signout, forgotPassword} = require('../controllers/auth');
 const {signupValidator} = require('../helpers/auth');
 
-router.post('/signup', isAuth, isAdmin, signupValidator, signup);
+router.post('/signup', signupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
 router.put('/password/forgot', forgotPassword);
