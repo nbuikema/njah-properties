3c07acc228393a625459ef3aa2e167ebae6afe6a
Nick Buikema <nick.buikema@gmail.com>
2019-12-30 14:39:08 -0600

    created build folder

---
+++ build/precache-manifest.690df1993bc8033c20b89c6d5c1d8d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3560ba9ca071c6211daf20ea537d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad9d0063ae98d2653152",
+    "url": "/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "ad9d0063ae98d2653152",
+    "url": "/static/js/2.5705454a.chunk.js"
+  },
+  {
+    "revision": "c1ec1e885aacfaa33159077a9cd1f5f1",
+    "url": "/static/js/2.5705454a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74080ed19a04490849b0",
+    "url": "/static/js/main.38b74615.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "b6e78d9ef8efc1109e815c1d06a6a719",
+    "url": "/static/media/logo.b6e78d9e.png"
+  }
+]);
