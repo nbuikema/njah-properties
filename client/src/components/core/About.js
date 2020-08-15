7e0d58761107a50921acc3273f64b963d2baf32a
nbuikema <46764298+nbuikema@users.noreply.github.com>
2020-08-14 19:59:31 -0500

    improved responsiveness

--- client/src/components/core/About.js
+++ client/src/components/core/About.js
@@ -9,24 +9,20 @@ const About = () => (
             <h1 className='text-center'>About Us</h1>
             <hr />
             <div className='row'>
-                <div className='col-xs-12 col-sm-7 align-self-center'>
-                    <h2>Who Are We?</h2>
-                    <br className='d-block d-sm-none' />
-                    <img className='d-block d-sm-none about-imgs' src={house} />
-                    <br />
+                <div className='col-md-12 col-lg-7 align-self-center'>
                     <p>
                         NJAH Properties is a privately owned group of properties offering comfortable, well-maintained accommodations to our tenants. We aim to provide quality living at an affordable, friendly environment. As a testament to this, most of our tenants have been with us for 2-15 years. Our team will work with you to provide you with the best living environment. We are a pet friendly property company. We look forward to working with you, if you find that one of our available properties will satisfy your housing needs.
                     </p>
                 </div>
-                <div className='col-xs-12 col-sm-5'>
-                    <img className='d-none d-sm-block about-imgs' src={house} />
+                <div className='col-md-12 col-lg-5'>
+                    <img className='about-imgs' src={house} />
                 </div>
             </div>
-            <br /><br />
+            <br /><br /><br />
             <div className='row text-center'>
                 <div className='col-12'>
                     <h2>Testimonials</h2>
-                    <br />
+                    <hr />
                     <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
                         <div className="carousel-inner">
                             <div className="carousel-item active">
@@ -59,20 +55,21 @@ const About = () => (
                     </div>
                 </div>
             </div>
-            <br /><br />
+            <br /><br /><br />
             <div className='row'>
-                <div className='col-12'>
+                <div className='col-12 text-center'>
                     <h2>Meet The Management</h2>
+                    <hr />
                 </div>
             </div>
             <br />
             <div className='row'>
-                <div className='col-xs-12 col-sm-6 border-right'>
+                <div className='col-sm-12 col-md-6 border-right'>
                     <div className='row'>
-                        <div className='col-xs-12 col-sm-6'>
+                        <div className='col-12 col-lg-6 align-self-center order-1 order-lg-0'>
                             <img className='about-imgs' src={allo} />
                         </div>
-                        <div className='col-xs-12 col-sm-6 text-right align-self-center'>
+                        <div className='col-12 col-lg-6 text-right align-self-center order-0 order-lg-1'>
                             <h4 className='mb-0'>Dr. Simon Allo</h4>
                             <em>Landlord</em>
                             <br /><br />
@@ -82,9 +79,9 @@ const About = () => (
                         </div>
                     </div>
                 </div>
-                <div className='col-xs-12 col-sm-6 border-left'>
+                <div className='col-sm-12 col-md-6 border-left'>
                     <div className='row'>
-                        <div className='col-xs-12 col-sm-6 align-self-center'>
+                        <div className='col-12 col-lg-6 align-self-center'>
                             <h4 className='mb-0'>Judy Zhu</h4>
                             <em>Office Manager</em>
                             <br /><br />
@@ -92,7 +89,7 @@ const About = () => (
                                 I am personally involved in the evaluation, inspection and purchase of our properties. I am also involved in the redesign and modifications of existing properties to make sure they meet our high standards. Our new structures are similar designed to meet the high expectations of our company.
                             </p>
                         </div>
-                        <div className='col-xs-12 col-sm-6'>
+                        <div className='col-12 col-lg-6 align-self-center'>
                             <img className='about-imgs' src={allo} />
                         </div>
                     </div>
